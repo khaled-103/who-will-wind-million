973cb728bedb50f32e8ce9ca33b168ec9e2dd00a
khaled Shiekh Ali <20khaled2001@gmail.com>
2025-10-18 11:51:12 +0300

    add react router and add primary folders project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './pages/home'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>} />
+        <Route path='/new-game' element={<div>new game</div>} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
